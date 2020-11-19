67ad47215eb1f7bb42a2aa5160c867d8f607e43e
oscarporfer <oscarfportes@gmail.com>
2020-11-18 19:18:11 -0500

    Added Schema, Model, and Routes

---
+++ server/db/models/user.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose'),
+  validator = require('validator'),
+  bcrypt = require('bcryptjs'),
+  jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema(
+  {
+    artistName: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid.');
+        }
+      }
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.toLowerCase().includes('password')) {
+          throw new Error("Password can't be password.");
+        }
+        if (value.length < 6) {
+          throw new Error('Password must be at least 6 characters long.');
+        }
+      }
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user._id.toString(), name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: '24h' }
+  );
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server/routes/open/index.js
+++ server/routes/open/index.js
@@ -1,11 +1,7 @@
-const router = require('express').Router();
+const router = require('express').Router(),
+  { createUser, loginUser } = require('../../controllers/users');
 
-// JUST FOR DEMO PURPOSES, PUT YOUR ACTUAL API CODE HERE
-router.get('/api/demo', (request, response) => {
-  response.json({
-    message: 'Hello from server.js'
-  });
-});
-// END DEMO
+router.post('/', createUser);
+router.post('/login', loginUser);
 
 module.exports = router;
