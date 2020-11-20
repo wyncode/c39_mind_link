5a855e2fc0f17a4b90e473b9166b8c0a03e430ce
oscarporfer <oscarfportes@gmail.com>
2020-11-20 00:47:27 -0500

    Reset Passwords Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppContextProvider } from './context/AppContext';
-// import ContextDemo from './components/ContextDemo';
 import ArtistPage from './Pages/ArtistPage';
 import Home from './Pages/HomePage';
 import CollabPage from './Pages/CollabPage';
@@ -8,18 +7,9 @@ import CollabDetails from './Pages/CollabDetails';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import ResetPassword from './Pages/ResetPassword';
 
 const App = () => {
-  // const [serverMessage, setServerMessage] = useState('');
-
-  // const fetchDemoData = () => {
-  //   fetch('/api/demo')
-  //     .then((response) => response.json())
-  //     .then((data) => setServerMessage(data.message));
-  // };
-
-  // useEffect(fetchDemoData, []);
-
   return (
     <AppContextProvider>
       <BrowserRouter>
@@ -27,16 +17,12 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/profile" component={ArtistPage} />
           <Route exact path="/collaboration" component={CollabPage} />
+          <Route exact path="/resetpassword" component={ResetPassword} />
           <Route
             exact
             path="/collaboration/details"
             component={CollabDetails}
           />
-          {/* <div id="demo">
-            <h3>Hello from client/src/App.js</h3>
-            <ContextDemo />
-            <h3>{serverMessage}</h3>
-          </div> */}
         </Switch>
       </BrowserRouter>
     </AppContextProvider>
---
+++ client/src/Pages/ResetPassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResetPasswords from '../components/ResetPage/ResetPasswords';
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <ResetPasswords />
+    </div>
+  );
+};
+
+export default ResetPassword;
