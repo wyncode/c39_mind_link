898eb977977249cf0c4f9aea3a437c07a62faf2d
Farah <farahcadet@gmail.com>
2020-11-27 19:48:11 -0500

    added sort button to project list

--- client/src/components/ProjectList/ProjectList.jsx
+++ client/src/components/ProjectList/ProjectList.jsx
@@ -4,30 +4,85 @@ import './ProjectList.css';
 import axios from 'axios';
 
 const ProjectList = () => {
-  const [project, setProject] = useState([]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
         const projectCards = await axios.get('/api/projects');
-        setProject(projectCards.data);
+        setProjects(projectCards.data);
       } catch (e) {
         console.log(e);
       }
     };
     fetchProjects();
-  }, [project]);
+  }, []);
+
+  const sortStarted1 = () => {
+    const array = [...projects].sort((a, b) =>
+      b.projectName.localeCompare(a.projectName)
+    );
+    setProjects(array);
+    console.log('sorted 1', projects);
+  };
+
+  const sortStarted2 = () => {
+    const array = [...projects].sort((a, b) =>
+      a.projectName.localeCompare(b.projectName)
+    );
+    setProjects(array);
+    console.log('sorted 2', projects);
+  };
+
+  const sortStarted3 = () => {
+    const array = projects.sort((a, b) =>
+      b.projectName.localeCompare(a.projectName) ? 1 : -1
+    );
+    setProjects(array);
+    console.log('sorted 3', projects);
+  };
 
   return (
     <div>
-      <div>Add sorting feature</div>
-      <div>Add findbyID to link to detail page</div>
+      <div>
+        {' '}
+        <button
+          style={{
+            color: 'white',
+            backgroundColor: 'black',
+            borderRadius: '15px'
+          }}
+          onClick={sortStarted1}
+        >
+          Sort by Start Date 1
+        </button>
+        <button
+          style={{
+            color: 'white',
+            backgroundColor: 'black',
+            borderRadius: '15px'
+          }}
+          onClick={sortStarted2}
+        >
+          Sort by Start Date 2
+        </button>
+        <button
+          style={{
+            color: 'white',
+            backgroundColor: 'black',
+            borderRadius: '15px'
+          }}
+          onClick={sortStarted3}
+        >
+          Sort by Start Date 3
+        </button>
+      </div>
       <div>Add styling</div>
       <div id="projectCardList">
-        {project.map((project) => {
+        {projects.map((project) => {
           return (
             <ProjectCard
-              key={project._id}
+              key={projects._id}
               project={project}
               className="projectCards"
             />
